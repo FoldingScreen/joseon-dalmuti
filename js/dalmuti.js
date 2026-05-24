18b08afce82840ad8b5c696f7febe3ddc808eecc
leechanyoung <leechanyoung90@gmail.com>
2026-05-25 00:41:52 +0900

    Update dalmuti.js

--- js/dalmuti.js
+++ js/dalmuti.js
@@ -1192,7 +1192,7 @@ installPassCountFix();
 installPresenceMessages();
 
 const scripts = [
-  "./js/00-config.js?v=20260524-dalmuti13",
+  "./js/00-config.js?v=20260524-dalmuti14",
 ];
 
   document.write(
